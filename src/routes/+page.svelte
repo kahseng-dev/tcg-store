f03dcb7223fe23eeb84770774408dfff29f71505
kahseng-dev <kahseng.chew@protonmail.com>
2025-02-26 10:28:04 +0800

    refactor: add boilerplate code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    import Navbar from "$lib/components/Navbar.svelte";
+
+    let project = $state("TCG Store")
+    let page = $state("Home")
+</script>
+
+<svelte:head>
+    <title>{page} | {project}</title>
+</svelte:head>
+
+<Navbar/>
+<p></p>
