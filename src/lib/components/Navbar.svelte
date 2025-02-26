dc5373985964d0f6e0934d5a56e23248b3cfda1e
kahseng-dev <kahseng.chew@protonmail.com>
2025-02-26 19:38:06 +0800

    refactor: parameters to prop rune

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
-    export let title;
+    let { title } = $props();
 </script>
 
 <div class="m-8 flex justify-between">
     <a href="./" class="text-2xl font-bold">{title}</a>
-    <button class="px-4 pb-1 border rounded-full ">
-        menu +
+    <button class="outline-2 rounded-full flex items-center px-4 pb-0.5">
+        menu
+        <i class="pl-1 ph ph-plus text-base"></i>
     </button>
 </div>
