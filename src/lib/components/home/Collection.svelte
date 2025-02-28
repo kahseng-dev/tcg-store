cc9cf540880f62139d61c71b8c1a5afea5836081
kahseng-dev <kahseng.chew@protonmail.com>
2025-02-28 19:14:32 +0800

    feat: add home collection component

---
+++ src/lib/components/home/Collection.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+</script>
+
+<div class="flex flex-col items-center">
+    <p class="font-bold text-2xl">Collection</p>
+    <div class="mt-8 px-24 w-full grid grid-flow-col grid-cols-2 grid-rows-2 text-white text-xl font-bold gap-8">
+        <div class="h-48 bg-zinc-500 flex items-center justify-center">
+            <p>Pokemon TCG</p>
+        </div>
+        <div class="h-48 bg-zinc-500 flex items-center justify-center">
+            <p>Pokemon TCG</p>
+        </div>
+        <div class="h-48 bg-zinc-500 flex items-center justify-center">
+            <p>Pokemon TCG</p>
+        </div>
+        <div class="h-48 bg-zinc-500 flex items-center justify-center">
+            <p>Pokemon TCG</p>
+        </div>
+    </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
     import Menu from "$components/Menu.svelte";
     import Notice from "$components/home/Notice.svelte";
     import ProductShowcase from "$components/home/ProductShowcase.svelte";
+    import Collection from "$components/home/Collection.svelte";
     import Stories from "$components/home/Stories.svelte";
     import Carousel from "$components/Carousel.svelte";
     import Footer from "$components/Footer.svelte";
@@ -34,6 +35,7 @@
         <Carousel items={store.products} />
     </div>
     <ProductShowcase />
+    <Collection />
     <div class="mt-8">
         <Stories />
     </div>
