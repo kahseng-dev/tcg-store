7369e63cb9c78bc93063f9371bce11828947655b
kahseng-dev <kahseng.chew@protonmail.com>
2025-02-28 11:35:30 +0800

    fix: stores component to stories file name change

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
     import Menu from "$components/Menu.svelte";
     import Announcement from "$components/home/Announcement.svelte";
-    import Stories from "$components/home/Stores.svelte";
+    import ProductShowcase from "$components/home/ProductShowcase.svelte";
+    import Stories from "$components/home/Stories.svelte";
     import Carousel from "$components/Carousel.svelte";
     import Footer from "$components/Footer.svelte";
 
@@ -32,6 +33,7 @@
     <div class="m-8">
         <Carousel items={store.products} />
     </div>
+    <ProductShowcase />
     <Stories />
 </div>
 <Footer store={store} />
