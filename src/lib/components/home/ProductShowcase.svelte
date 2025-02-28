ad6c63e12e1af52c3b8d65201870646876f1aeec
kahseng-dev <kahseng.chew@protonmail.com>
2025-02-28 11:35:44 +0800

    feat: add home product showcase component

---
+++ src/lib/components/home/ProductShowcase.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+</script>
+
+<div class="flex flex-col items-center">
+    <div class="flex font-mono uppercase text-xs gap-4 tracking-widest font-semibold text-zinc-500">
+        <p>Best Sellers</p>
+        <p>New Arrivals</p>
+    </div>
+    <div class="my-8 flex gap-12">
+        <div class="flex flex-col items-center">
+            <div class="w-48 h-48 bg-zinc-500"></div>
+            <p class="mt-4 font-bold">item name</p>
+            <p class="mt-2 text-zinc-500">item subtext</p>
+            <p class="mt-4">item price</p>
+            <button class="mt-4 bg-yellow-500 px-8 py-2 rounded">Add to Cart</button>
+        </div>
+        <div class="flex flex-col items-center">
+            <div class="w-48 h-48 bg-zinc-500"></div>
+            <p class="mt-4 font-bold">item name</p>
+            <p class="mt-2 text-zinc-500">item subtext</p>
+            <p class="mt-4">item price</p>
+            <button class="mt-4 bg-yellow-500 px-8 py-2 rounded">Add to Cart</button>
+        </div>
+        <div class="flex flex-col items-center">
+            <div class="w-48 h-48 bg-zinc-500"></div>
+            <p class="mt-4 font-bold">item name</p>
+            <p class="mt-2 text-zinc-500">item subtext</p>
+            <p class="mt-4">item price</p>
+            <button class="mt-4 bg-yellow-500 px-8 py-2 rounded">Add to Cart</button>
+        </div>
+        <div class="flex flex-col items-center">
+            <div class="w-48 h-48 bg-zinc-500"></div>
+            <p class="mt-4 font-bold">item name</p>
+            <p class="mt-2 text-zinc-500">item subtext</p>
+            <p class="mt-4">item price</p>
+            <button class="mt-4 bg-yellow-500 px-8 py-2 rounded">Add to Cart</button>
+        </div>
+    </div>
+</div>
