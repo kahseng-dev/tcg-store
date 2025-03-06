b884a8134a954ccf2e26df9b7aca9873cf3d13f8
kahseng-dev <kahseng.chew@protonmail.com>
2025-03-06 20:17:24 +0800

    refactor: rename filteredOptions to limitedOptions

--- src/lib/components/RadioGroup.svelte
+++ src/lib/components/RadioGroup.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
     let { name, options, showLimit = "0" } = $props();
 
-    let filteredOptions = $state(options);
+    let limitedOptions = $state(options);
     let isChecked = $state(options[0]);
 
     if (showLimit != "0") {
-        filteredOptions = options.slice(0, showLimit);
+        limitedOptions = options.slice(0, showLimit);
     }
 
     function handleOptionClick(option:any) {
@@ -14,7 +14,7 @@
 </script>
 
 <div class="flex flex-col font-normal">
-    {#each filteredOptions as option}
+    {#each limitedOptions as option}
         <label class="flex gap-2 cursor-pointer">
             <input onclick={() => handleOptionClick(option)} type="radio" name="{name}" value="{option}" checked={isChecked === option} />
             {option}
