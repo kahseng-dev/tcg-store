c9a65ee9c534493c63da8f4ef751ecfc91b7bd02
kahseng-dev <kahseng.chew@protonmail.com>
2025-03-08 11:35:48 +0800

    fix: path incorrectly displaying route params instead of id

--- src/lib/components/Breadcrumb.svelte
+++ src/lib/components/Breadcrumb.svelte
@@ -3,18 +3,17 @@
 
     let urls: string[]
 
-    urls = page.route.id?.split("/") || []
-    urls.shift();
+    urls = page.url.pathname.split("/").slice(1);
 </script>
 
-<p class="uppercase text-xs text-zinc-500 tracking-wide">
+<div class="flex font-normal uppercase text-xs text-zinc-500 tracking-wide gap-1">
     <a class="font-mono hover:underline" href="./">Home</a>
     {#each urls as url}
-        <span class="font-mono font-normal">/</span>
-        {#if urls.at(-1) == url}
-            <span class="font-mono text-black">{url}</span>
+        <span class="font-mono">/</span>
+        {#if urls.at(-1) != url}
+            <a class="font-mono hover:underline" href="./" aria-label="{url}">{url}</a>
             {:else}
-            <span class="font-mono">{url}</span>
+            <span class="font-mono text-black">{url}</span>
         {/if}
     {/each}
-</p>
+</div>
