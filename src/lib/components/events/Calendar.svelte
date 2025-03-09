7e5021803c2abdf6ff3f630d82eb8d752667074a
kahseng-dev <kahseng.chew@protonmail.com>
2025-03-09 16:21:48 +0800

    feat: add calendar function base

--- src/lib/components/events/Calendar.svelte
+++ src/lib/components/events/Calendar.svelte
@@ -1,28 +1,56 @@
 <script lang="ts">
-    import { events, type Event } from "$lib/data/events";
+    import { events } from "$lib/data/events";
     import Button from "$lib/components/Button.svelte"
 
-    let days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
-    let numberOfDaysDisplayed = 35;
-    let today: Date = $state(new Date());
-    let month = $state(today.toLocaleString("default", { month: "long" }));
-    let year = $state(today.getFullYear().toString());
+    const monthNames = ['January','Febuary','March','April','May','June','July','August','September','October','November','December'];
+    const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    const uniqueCategories = [...new Set(events.map(event => event.category))];
+
+    let today:Date = $state(new Date());
+    let month:number = $state(today.getMonth());
+    let year:number = $state(today.getFullYear());
+
+    let hiddenPreviousMonth:number[] = $state([]);
+    let hiddenNextMonth:number[] = $state([]);
+    getHiddenDates();
+
+    let monthLastDay = $derived(new Date(year, month + 1, 0).getDate());
+    
+    function getHiddenDates() {
+        hiddenPreviousMonth = [];
+        hiddenNextMonth = [];
+
+        let monthStart = new Date(year, month, 1).getDay();
+        let nextMonthLastDate = new Date(year, month + 1, 0).getDate();
+        let monthLastDay = new Date(year, month, nextMonthLastDate).getDay();
+        let previousMonthLastDate = new Date(year, month, 0).getDate();
+
+        for (let i = monthStart; i > 0; i--) {
+            hiddenPreviousMonth.push(previousMonthLastDate - i + 1);
+        };
+
+        for (let i = monthLastDay; i < 6; i++) {
+            hiddenNextMonth.push(i - monthLastDay + 1);
+        };
+    }
 
     function nextMonth() {
-        today.setMonth(today.getMonth() + 1);
-        month = today.toLocaleString("default", { month: "long" });
-        year = today.getFullYear().toString();
+        today.setMonth(month + 1);
+        month = today.getMonth();
+        year = today.getFullYear();
+        getHiddenDates()
     }
 
     function previousMonth() {
-        today.setMonth(today.getMonth() - 1);
-        month = today.toLocaleString("default", { month: "long" });
-        year = today.getFullYear().toString();
+        today.setMonth(month - 1);
+        month = today.getMonth();
+        year = today.getFullYear();
+        getHiddenDates()
     }
 </script>
 
 <div class="flex flex-col gap-2">
-    <p class="text-2xl">{month} {year}</p>
+    <p class="text-2xl">{monthNames[month]} {year}</p>
     <div class="flex gap-2">
         <Button onclick={previousMonth} icon="arrow-left" class="w-min bg-transparent text-base hover:text-white" color="black"/>
         <Button onclick={nextMonth} icon="arrow-right" class="w-min bg-transparent text-base hover:text-white" color="black"/>
@@ -33,8 +61,27 @@
         {/each}
     </div>
     <div class="grid grid-cols-{days.length} border-l border-t border-zinc-300">
-        {#each { length: numberOfDaysDisplayed }, day}
-            <div class="col-span-1 h-24 text-black px-2 border-r border-b border-zinc-300">{day + 1}</div>
+        {#each hiddenPreviousMonth as previousDay}
+            <div class="col-span-1 h-24 text-zinc-500 px-2 border-r border-b border-zinc-300">
+                {previousDay}
+            </div>
+        {/each}
+        {#each { length: monthLastDay }, day}
+            <div class="col-span-1 h-24 text-black px-2 border-r border-b border-zinc-300">
+                {day + 1}
+            </div>
+        {/each}
+        {#each hiddenNextMonth as nextDay}
+            <div class="col-span-1 h-24 text-zinc-500 px-2 border-r border-b border-zinc-300">
+                {nextDay}
+            </div>
+        {/each}
+    </div>
+    <div class="mt-4 grid grid-cols-3 border-l border-t border-zinc-300">
+        {#each uniqueCategories as category}
+            <div class="col-span-1 h-24 border-r border-b border-zinc-300">
+                <p class="text-xl p-4 text-black">{category}</p>
+            </div>
         {/each}
     </div>
 </div>
--- src/lib/data/events.ts
+++ src/lib/data/events.ts
@@ -1,23 +1,35 @@
 export type Event = {
-	date: Date;
-	name: string;
-	text: string;
+	datetime: Date;
+	title: string;
+	category: string;
 }
 
 export const events:Event[] = [
 	{
-		date: new Date(),
-		name: "Yu Nagaba x Pokémon Booster Pack",
-		text: "Pokemon TCG"
+		datetime: new Date(),
+		title: "SV08 New Expansion",
+		category: "Pokemon TCG"
+
+	},
+	{
+		datetime: new Date(),
+		title: "Gym Store Event",
+		category: "Pokemon TCG"
+
 	},
     {
-		date: new Date(),
-		name: "Yu Nagaba x Pokémon Booster Pack",
-		text: "Pokemon TCG"
+		datetime: new Date(),
+		title: "Meetup Event",
+		category: "One Piece TCG"
+	},
+	{
+		datetime: new Date(),
+		title: "Standard Battle",
+		category: "One Piece TCG"
 	},
     {
-		date: new Date(),
-		name: "Yu Nagaba x Pokémon Booster Pack",
-		text: "Pokemon TCG"
+		datetime: new Date(),
+		title: "Tournament (Monthly)",
+		category: "Hololive"
 	}
 ];
